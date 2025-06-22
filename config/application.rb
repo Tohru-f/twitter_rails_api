03881701b8b890aa7f63bfcdd114a5817a153a6f
Tohru Furusawa <tohru.furusawa@gmail.com>
2025-06-23 06:52:45 +0900

    modify application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,14 +6,14 @@ require 'rails/all'
 
 require 'devise'
 
+require 'omniauth'
+require 'omniauth-google-oauth2'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Myapp
   class Application < Rails::Application
-    require 'omniauth'
-    require 'omniauth-google-oauth2'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
