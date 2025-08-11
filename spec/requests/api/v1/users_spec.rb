a7092e8124f25d87bd899b058b0e84f4543269a5
Tohru Furusawa <tohru.furusawa@gmail.com>
2025-08-11 10:03:04 +0900

    modify users_spec.rb for pending test

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -4,16 +4,18 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::Users' do
   describe 'GET /show' do
-    it 'returns http success' do
-      get '/api/v1/users/show'
-      expect(response).to have_http_status(:success)
-    end
+    # it 'returns http success' do
+      pending "add some examples (or delete) #{__FILE__}"
+      # get '/api/v1/users/show'
+      # expect(response).to have_http_status(:success)
+    # end
   end
 
   describe 'GET /update' do
-    it 'returns http success' do
-      get '/api/v1/users/update'
-      expect(response).to have_http_status(:success)
-    end
+    # it 'returns http success' do
+      pending "add some examples (or delete) #{__FILE__}"
+      # get '/api/v1/users/update'
+      # expect(response).to have_http_status(:success)
+    # end
   end
 end
