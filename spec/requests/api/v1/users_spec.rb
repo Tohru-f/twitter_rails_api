ade8be5f3f64b91eab18b6fe2c96f905429e3552
Tohru Furusawa <tohru.furusawa@gmail.com>
2025-08-11 10:06:45 +0900

    reflect modification of rubocop

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -5,17 +5,17 @@ require 'rails_helper'
 RSpec.describe 'Api::V1::Users' do
   describe 'GET /show' do
     # it 'returns http success' do
-      pending "add some examples (or delete) #{__FILE__}"
-      # get '/api/v1/users/show'
-      # expect(response).to have_http_status(:success)
+    pending "add some examples (or delete) #{__FILE__}"
+    # get '/api/v1/users/show'
+    # expect(response).to have_http_status(:success)
     # end
   end
 
   describe 'GET /update' do
     # it 'returns http success' do
-      pending "add some examples (or delete) #{__FILE__}"
-      # get '/api/v1/users/update'
-      # expect(response).to have_http_status(:success)
+    pending "add some examples (or delete) #{__FILE__}"
+    # get '/api/v1/users/update'
+    # expect(response).to have_http_status(:success)
     # end
   end
 end
