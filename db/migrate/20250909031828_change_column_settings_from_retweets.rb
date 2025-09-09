46c5bbffbbe3c5b29f27219908a47ba62a6a3d29
Tohru Furusawa <tohru.furusawa@gmail.com>
2025-09-10 06:22:49 +0900

    add combined unique key to retweest table

---
+++ db/migrate/20250909031828_change_column_settings_from_retweets.rb
@@ -0,0 +1,11 @@
+class ChangeColumnSettingsFromRetweets < ActiveRecord::Migration[7.0]
+  def up
+    add_index :retweets, %i[user_id tweet_id], unique: true
+  end
+
+  def down
+    # remove_index :retweets, column: :user_id
+    # remove_index :retweets, column: :tweet_id
+    remove_index :retweets, %i[user_id tweet_id]
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_09_07_013757) do
+ActiveRecord::Schema[7.0].define(version: 2025_09_09_031828) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,8 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_07_013757) do
     t.bigint "tweet_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["tweet_id"], name: "index_retweets_on_tweet_id"
-    t.index ["user_id"], name: "index_retweets_on_user_id"
+    t.index ["user_id", "tweet_id"], name: "index_retweets_on_user_id_and_tweet_id", unique: true
   end
 
   create_table "tasks", force: :cascade do |t|
